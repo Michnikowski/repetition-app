d58d23f8a51ff36d9549d3a4b0603bc6ddaa53ab
Michnikowski <michal.gwiazdonik@gmail.com>
2021-05-25 21:56:17 +0200

    Add e2e configuration

---
+++ jest.config-e2e.js
@@ -0,0 +1,16 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  verbose: true,
+};
